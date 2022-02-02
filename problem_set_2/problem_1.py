a0a988e9a0f179411afa00767d31ddb125056c0c
lucastkacz <lucastkacz@gmail.com>
2022-02-02 20:57:25 -0300

    Finished problem 2

--- problem_set_2/problem_1.py
+++ problem_set_2/problem_1.py
@@ -6,7 +6,7 @@ The following variables contain values as described below:
     >>> monthlyPaymentRate - minimum monthly payment rate as a decimal
 """
 
-balance = 484
+balance = 4773
 annualInterestRate = 0.20
 monthlyPaymentRate = 0.04
 
@@ -14,7 +14,7 @@ amount_periods = 12
 rate = annualInterestRate / amount_periods
 
 balance_i = balance
-for i in range(amount_periods):
+for _ in range(amount_periods):
     payment_i = balance_i * monthlyPaymentRate
     u_balance_i = balance_i - payment_i
     interest_i = (rate * u_balance_i)
---
+++ problem_set_2/problem_2.py
@@ -0,0 +1,27 @@
+"""
+Now write a program that calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months. 
+By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant amount that will be paid each month.
+"""
+
+balance = 4773
+annualInterestRate = 0.20
+
+amount_periods = 12
+rate = annualInterestRate / amount_periods
+
+def calculate_balance_fixed(balance, rate, fixedPaymet):
+    balance_i = balance
+    payment_i = fixedPaymet
+    for _ in range(amount_periods):
+        u_balance_i = balance_i - payment_i
+        interest_i = (rate * u_balance_i)
+        balance_i = u_balance_i + interest_i
+    return balance_i
+
+
+lowest_payment = 10
+while calculate_balance_fixed(balance, rate, lowest_payment) > 0:
+    lowest_payment += 10
+
+
+print('Lowest Payment: %s' % lowest_payment)
